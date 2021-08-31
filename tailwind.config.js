c00e6ac06a5dac7ad6cd22d45d5c50fb6f8a615e
macklark <ganesh.kolavennu@gmail.com>
2021-08-31 10:03:09 +0530

    added new theme for extended bubble animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        bubble: {
+          "0%": {
+            transform: "scale(1)",
+          },
+          "33%": {
+            transform: "scale(1.1)",
+          },
+          "66%": {
+            transform: "scale(0.9)",
+          },
+          "100%": {
+            transform: "scale(1)",
+          },
+        },
+      },
+      animation: {
+        bubble: "bubble 7s infinite",
+      },
+    },
   },
   variants: {
     extend: {},
