70dfd7ff3eda277d73de26d2501aecee2c1ecf98
macklark <ganesh.kolavennu@gmail.com>
2021-08-31 10:02:20 +0530

    route transition animation change

--- pages/_app.js
+++ pages/_app.js
@@ -5,9 +5,9 @@ function MyApp({ Component, pageProps, router }) {
   return (
     <motion.div
       key={router.route}
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 1 }}
+      initial={{ scale: 0 }}
+      animate={{ scale: 1 }}
+      transition={{ duration: 0.5 }}
     >
       <Component {...pageProps} />
     </motion.div>
